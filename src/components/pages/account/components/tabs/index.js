ece9863cd51349cdea0e1fef081d34407868db2d
Artur Vieira <vieira.artur.g@gmail.com>
2018-01-28 20:22:41 -0800

    Edits to polish tab component

--- src/components/pages/account/components/tabs/index.js
+++ src/components/pages/account/components/tabs/index.js
@@ -8,23 +8,37 @@ class Tabs extends Component {
         this.setTabs();
     }
 
-    setTabs() {
+    setTabs(index) {
+        let activeIndex = null;
+        let body = null;
+        if (!index) {
+            activeIndex = this.props.children.reduce((acc, next, index) => {
+                if (next.props.active) acc = index;
+                return acc;
+            }, 0);
+            body = this.props.children.filter(node => node.props.active);
+        } else {
+            activeIndex = index;
+            body = this.props.children[index];
+        }
         const titles = this.props.children
             .map(node => node.props.label)
             .map((title, index) => (
-                <h3 key={index} onClick={() => this.handleClick(index)}>
+                <h3
+                    key={index}
+                    className={`titles ${
+                        index === activeIndex ? 'active' : ''
+                    }`}
+                    onClick={() => this.handleClick(index)}
+                >
                     {title}
                 </h3>
             ));
-
-        const body = this.props.children.filter(node => node.props.active);
         this.setState({ titles: titles, body: body });
     }
 
     handleClick = index => {
-        console.log(index);
-        const body = this.props.children[index];
-        this.setState({ body: body });
+        this.setTabs(index);
     };
 
     render = () => {
